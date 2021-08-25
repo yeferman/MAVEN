ef3c01416a316caf225989df6a6e19ec56eae8ec
yeferman <77856505+yeferman@users.noreply.github.com>
2021-08-25 16:19:45 -0500

    Add files via upload

---
+++ maven/src/test/java/com/example/MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
